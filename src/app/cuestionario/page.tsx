2f70e4ac5d6cde8204c3819386921e51e4cd8356
Ivan Martinez <jesulim@gmail.com>
2024-10-04 06:14:32 -0400

    Refactor landing component and add CuestionarioPage

---
+++ src/app/cuestionario/page.tsx
@@ -0,0 +1,11 @@
+import { Cuestionario2 } from '@/components/component/cuestionario2';
+
+const CuestionarioPage = () => {
+  return (
+    <>
+      <Cuestionario2 />
+    </>
+  );
+};
+
+export default CuestionarioPage;
